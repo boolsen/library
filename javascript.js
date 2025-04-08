a0ee41560c2d45208672bdd6fe64ea9ccc08f7bf
Audun Kronstad <141585731+boolsen@users.noreply.github.com>
2025-04-08 19:02:04 +0200

    boilerplate setup, book object setup

---
+++ javascript.js
@@ -0,0 +1,29 @@
+const myLibrary = [];
+
+function Book(title, author, pageCount, read) {
+
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.alreadyRead = read;
+    this.id = crypto.randomUUID();
+
+    this.info = function() {
+        return this.title + " by " + this.author + ", " + this.pages + ", " + this.alreadyRead;
+    }
+}
+
+function addBookToLibrary(title) {
+    let book = new Book;
+    myLibrary.push(book);
+}
+
+function displayLibrary() {
+    for (book in myLibrary) {
+        console.log(book.getTitle());
+    }
+}
